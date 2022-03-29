17700b76e530d43fe21e73ed3a14bff9c531de55
Eoghann Veaute <o2181406@campus.univ-orleans.fr>
2022-03-29 15:54:27 +0200

    utilisation de l'api pour le login et le register

--- TimeTracker/ViewModels/LoginViewModel.cs
+++ TimeTracker/ViewModels/LoginViewModel.cs
@@ -4,11 +4,37 @@ using System.Collections.Generic;
 using System.Text;
 using System.Windows.Input;
 using Xamarin.Forms;
+using TimeTracker.API;
+using TimeTracker.API.Authentications;
+using TimeTracker.API.ThrowException;
 
 namespace TimeTracker
 {
     public class LoginViewModel : ViewModelBase
     {
+        private ApiTest api;
+        private String _email;
+        private String _password;
+
+        public String Email { 
+            get { return _email; } 
+            set { SetProperty(ref _email, value); } 
+        }
+
+        public String Password
+        {
+            get { return _password; }
+            set { SetProperty(ref _password, value); }
+        }
+
+        private bool _error;
+
+        public bool Error
+        {
+            get { return _error; }
+            set { SetProperty(ref _error, value); }
+        }
+
         public ICommand Register
         {
             get;
@@ -26,16 +52,19 @@ namespace TimeTracker
             try
             {
                 //TODO Requete POST sur /api/v1/login (si token expiré ?) pour récupérer un token d'accès
+                Response<LoginResponse> test = await api.loginAsync(_email, _password);
                 await NavigationService.PushAsync<MainPage>();
             }
-            catch (Exception ex)
+            catch (UserNotFoundException ex)
             {
-                Console.WriteLine(ex);
+                Error = true;
             }
         }
 
         public LoginViewModel()
         {
+            api = new ApiTest();
+            _error = false;
             Register = new Command(goToRegister);
             Confirm = new Command(goToMain);
         }
--- TimeTracker/ViewModels/RegisterViewModel.cs
+++ TimeTracker/ViewModels/RegisterViewModel.cs
@@ -3,24 +3,79 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows.Input;
+using TimeTracker.API;
+using TimeTracker.API.Authentications;
+using TimeTracker.API.ThrowException;
 using Xamarin.Forms;
 
 namespace TimeTracker
 {
     public class RegisterViewModel : ViewModelBase
     {
+        private ApiTest api;
+        private String _firstname;
+        private String _lastname;
+        private String _email;
+        private String _password;
+
+        public String Firstname
+        {
+            get { return _firstname; }
+            set { SetProperty(ref _firstname, value); }
+        }
+        public String Lastname
+        {
+            get { return _lastname; }
+            set { SetProperty(ref _lastname, value); }
+        }
+        public String Email
+        {
+            get { return _email; }
+            set { SetProperty(ref _email, value); }
+        }
+        public String Password
+        {
+            get { return _password; }
+            set { SetProperty(ref _password, value); }
+        }
+
         public ICommand Confirm { get; }
         public ICommand Back { get; }
+
+        private bool _error;
+
+        private String _textError;
+
+        public String TextError
+        {
+            get { return _textError; }
+            set { SetProperty(ref _textError, value); }
+        }
+
+        public bool Error
+        {
+            get { return _error; }
+            set { SetProperty(ref _error, value); }
+        }
 
         public async void goToMain()
         {
             try
             {
-                //TODO Créer le compte avec requete POST sur /api/v1/register et dans le modèle 
-                await NavigationService.PushAsync<MainPage>();
+                if (_firstname.Length > 0 && _lastname.Length>0 && _email.Length>0 && _password.Length>0)
+                {
+                    //TODO Créer le compte avec requete POST sur /api/v1/register et dans le modèle 
+                    Response<LoginResponse> test = await api.registerAsync(_email, _firstname, _lastname, _password);
+                    await NavigationService.PushAsync<MainPage>();
+                }
+                else{
+                    TextError = "All fields must be completed";
+                    Error = true;
+                }
             }
-            catch (Exception ex) { 
-                Console.WriteLine(ex);
+            catch (MailAlreadyExistException ex) {
+                TextError = "Mail already used";
+                Error = true;
             }
         }
 
@@ -28,7 +83,7 @@ namespace TimeTracker
         {
             try
             { 
-                await NavigationService.PushAsync<Login>();
+                await NavigationService.PopAsync(true);
             }
             catch (Exception ex)
             {
@@ -38,8 +93,14 @@ namespace TimeTracker
 
         public RegisterViewModel()
         {
+            _error = false;
+            api = new ApiTest();
             Confirm = new Command(goToMain);
             Back = new Command(goToLogin);
+            _firstname = "";
+            _lastname = "";
+            _email = "";
+            _password = "";
         }
     }
 }
